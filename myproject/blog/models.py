e2afcfe38cc05ee3423f3783c599fe2bfef8d054
mostafa <samishakoori7@gmail.com>
2020-08-24 20:24:12 +0430

    converting calandar to jalali calander

--- myproject/blog/models.py
+++ myproject/blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from extensions.utils import jalali_converter
 
 # Create your models here.
 class Article(models.Model):
@@ -22,3 +23,7 @@ class Article(models.Model):
 
     def __str__(self):
         return self.title
+
+    def jpublish(self):
+        return jalali_converter(self.publish)
+    jpublish.short_description = 'زمان انتشار'
