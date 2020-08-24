16f87d3c322e4b79f93201e97b118c2f5c9acc89
mostafa <samishakoori7@gmail.com>
2020-08-24 21:05:07 +0430

    creating category model and register to admin.py its adjasting in admin panel

--- myproject/blog/models.py
+++ myproject/blog/models.py
@@ -3,6 +3,19 @@ from django.utils import timezone
 from extensions.utils import jalali_converter
 
 # Create your models here.
+class Category(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=100, unique=True)
+    status = models.BooleanField(default=True, verbose_name='آیا نمایش داده شود؟')
+    position = models.IntegerField()
+    class Meta:
+        verbose_name = 'دسته بندی'
+        verbose_name_plural = 'دسته بندی ها'
+        ordering = ['position']
+
+    def __str__(self):
+        return self.title
+
 class Article(models.Model):
     STATUS_CHOICES = (
         ('d', 'Draft'),
@@ -10,6 +23,7 @@ class Article(models.Model):
     )
     title = models.CharField(max_length=200)
     slug = models.SlugField(max_length=100, unique=True)
+    category = models.ManyToManyField(Category, verbose_name='دسته بندی')
     description = models.TextField()
     thumbnail = models.ImageField(upload_to='images')
     publish = models.DateTimeField(default=timezone.now)
