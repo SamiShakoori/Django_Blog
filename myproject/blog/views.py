186bc41f1cdcd44aab542ea16acf12764b23de11
mostafa <samishakoori7@gmail.com>
2020-08-24 14:15:48 +0430

    adding context statement for recieving data from database in html page

--- myproject/blog/views.py
+++ myproject/blog/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
+from .models import Article
 
 # Create your views here.
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'articles': Article.objects.filter(status='p').order_by('-publish')
+    }
+    return render(request, 'blog/home.html', context)
