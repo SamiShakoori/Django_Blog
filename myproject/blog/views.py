49ac5f864011c4c0ffe134caa17dbd37d7eec2e7
mostafa <samishakoori7@gmail.com>
2020-08-29 14:24:51 +0430

    creating paginator for home view and category view and adding new article

--- myproject/blog/views.py
+++ myproject/blog/views.py
@@ -1,11 +1,17 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
 # from django.http import HttpResponse
 from .models import Article, Category
 
 # Create your views here.
-def home(request):
+def home(request, page=1):
+    articles_list = Article.objects.filter(status='p').order_by('-publish')
+    paginator = Paginator(articles_list, 2)
+    # after adding page in url home then we do not need to this syntax
+    # page = request.GET.get('page')
+    articles = paginator.get_page(page)
     context = {
-        'articles': Article.objects.filter(status='p').order_by('-publish'),
+        'articles': articles,
         # 'category': Category.objects.filter(status=True)
     }
     return render(request, 'blog/home.html', context)
@@ -16,8 +22,13 @@ def detail(request, slug):
     }
     return render(request, 'blog/detail.html', context)
 
-def category(request, slug):
+def category(request, slug, page=1):
+    category = get_object_or_404(Category, slug=slug, status=True)
+    article_list = category.articles.published()
+    paginator = Paginator(article_list, 2)
+    articles = paginator.get_page(page)
     context = {
-        'category': get_object_or_404(Category, slug=slug, status=True)
+        'category': category,
+        'articles': articles
     }
     return render(request, 'blog/category.html', context)
